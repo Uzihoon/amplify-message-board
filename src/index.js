9e05bb6e2e4a44d5407a1bff3eb3760e453bab0a
uzihoon <uzihoon.dev@gmail.com>
2021-07-23 16:21:27 +0900

    Update message board component

--- src/index.js
+++ src/index.js
@@ -3,6 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Amplify from '@aws-amplify/core';
+import config from './aws-exports';
+import 'antd/dist/antd.css';
+
+Amplify.configure(config);
 
 ReactDOM.render(
   <React.StrictMode>
